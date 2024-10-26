015f8e8285a26c97cedba2c3745e59ca16e4d1ff
anwarkurbaj <anwrkurbaj@gmail.com>
2024-10-26 20:03:34 +0800

    add home app in database

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import home   # .: the admin and model in same folder
+
+admin.site.register(home)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+'''
+django model fields :
+1-html widjet 
+2-valdition
+3-db size
+'''
+# Create your models here.# taple in data base =class
+# coulmn in data base = filed 
+class home(models.Model):# inhertans to acsses filed
+    title =models.CharField(max_length=100)# like coulmn in data base 
+    
